c4b4df9d987f18414f9a7640b1062fe122557c2c
David Cristofaro <david@dtcristo.com>
2021-08-07 00:20:45 +1000

    WIP

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ use bevy::{
     window::{WindowMode, WindowResizeConstraints},
 };
 use bevy_pixels::prelude::*;
-use glam::{vec2, vec3, Affine3A, Mat4, Vec2, Vec3};
+use glam::{vec3, Affine3A, Mat4, Vec3};
+use image::{io::Reader as ImageReader, DynamicImage};
 
 const WIDTH: u32 = 320;
 const HEIGHT: u32 = 240;
@@ -58,6 +59,7 @@ pub struct AppState {
     position: Position,
     velocity: Velocity,
     direction: Direction,
+    brick: DynamicImage,
 }
 
 #[derive(Debug, Hash, PartialEq, Eq, Clone, StageLabel)]
@@ -67,9 +69,11 @@ enum AppStage {
 }
 
 fn main() {
+    let brick = ImageReader::open("brick.png").unwrap().decode().unwrap();
+
     App::build()
         .insert_resource(WindowDescriptor {
-            title: "prber".to_string(),
+            title: "prender".to_string(),
             width: (4 * WIDTH) as f32,
             height: (4 * HEIGHT) as f32,
             vsync: true,
@@ -87,9 +91,10 @@ fn main() {
         })
         .insert_resource(AppState {
             view: View::FirstPerson2d,
-            position: Position(vec3(0.0, 1.8, 0.0)),
+            position: Position(vec3(0.0, 2.0, 0.0)),
             velocity: Velocity(vec3(0.0, 0.0, 0.0)),
             direction: Direction(0.0),
+            brick: brick,
         })
         .add_plugins(DefaultPlugins)
         .add_plugin(PixelsPlugin)
@@ -259,46 +264,72 @@ fn draw_wall_system(
     let perspective = Mat4::perspective_infinite_rh(std::f32::consts::FRAC_PI_2, ASPECT_RATIO, 1.0);
 
     for wall in query.iter() {
-        let w_a_bottom = vec3(wall.a_position.0.x, 0.0, wall.a_position.0.z);
-        let w_a_top = vec3(wall.a_position.0.x, 5.0, wall.a_position.0.z);
-        let w_b_bottom = vec3(wall.b_position.0.x, 0.0, wall.b_position.0.z);
-        let w_b_top = vec3(wall.b_position.0.x, 5.0, wall.b_position.0.z);
+        let w_a_b = vec3(wall.a_position.0.x, 0.0, wall.a_position.0.z);
+        let w_a_t = vec3(wall.a_position.0.x, 4.0, wall.a_position.0.z);
+        let w_b_b = vec3(wall.b_position.0.x, 0.0, wall.b_position.0.z);
+        let w_b_t = vec3(wall.b_position.0.x, 4.0, wall.b_position.0.z);
 
-        let v_a_bottom = view.transform_point3(w_a_bottom);
-        let v_a_top = view.transform_point3(w_a_top);
-        let v_b_bottom = view.transform_point3(w_b_bottom);
-        let v_b_top = view.transform_point3(w_b_top);
+        let mut v_a_b = view.transform_point3(w_a_b);
+        let mut v_b_b = view.transform_point3(w_b_b);
 
-        let p_a_bottom = perspective.project_point3(v_a_bottom);
-        let p_a_top = perspective.project_point3(v_a_top);
-        let p_b_bottom = perspective.project_point3(v_b_bottom);
-        let p_b_top = perspective.project_point3(v_b_top);
+        println!("---------------------------------------------");
+        dbg!(state.position.0);
+        // dbg!(v_a_b);
+        // dbg!(v_b_b);
 
-        dbg!(v_a_bottom);
-        dbg!(v_b_bottom);
+        // if v_a_b.z >= -1.0 && v_b_b.z >= -1.0 {
+        //     // Wall entirely behind view plane, skip drawing
+        //     continue;
+        // } else if !(v_a_b.z < -1.0 && v_b_b.z < -1.0) {
+        //     // Wall intersects view plane
+        //     if v_a_b.z < -1.0 {
+        //         let z_ratio = (v_b_b.z / (v_a_b.z.abs() + v_b_b.z.abs())).abs();
+        //         dbg!(z_ratio);
+        //         v_b_b.x = v_b_b.x - z_ratio * (v_a_b.x - v_b_b.x);
+        //         v_b_b.z = -1.0;
+        //     } else {
+        //         let z_ratio = (v_a_b.z / (v_a_b.z.abs() + v_b_b.z.abs())).abs();
+        //         dbg!(z_ratio);
+        //         v_a_b.x = v_a_b.x - z_ratio * (v_b_b.x - v_a_b.x);
+        //         v_a_b.z = -1.0;
+        //     }
+        //     dbg!(v_a_b);
+        //     dbg!(v_b_b);
+        // }
+
+        let v_a_t = view.transform_point3(w_a_t);
+        let v_b_t = view.transform_point3(w_b_t);
+
+        let p_a_b = perspective.project_point3(v_a_b);
+        let p_a_t = perspective.project_point3(v_a_t);
+        let p_b_b = perspective.project_point3(v_b_b);
+        let p_b_t = perspective.project_point3(v_b_t);
+
+        // dbg!(p_a_b);
+        // dbg!(p_b_b);
 
         draw_line(
             frame,
-            normalized_to_pixel(p_a_top),
-            normalized_to_pixel(p_b_top),
+            normalized_to_pixel(p_a_t),
+            normalized_to_pixel(p_b_t),
             wall.color,
         );
         draw_line(
             frame,
-            normalized_to_pixel(p_a_bottom),
-            normalized_to_pixel(p_b_bottom),
+            normalized_to_pixel(p_a_b),
+            normalized_to_pixel(p_b_b),
             wall.color,
         );
         draw_line(
             frame,
-            normalized_to_pixel(p_a_top),
-            normalized_to_pixel(p_a_bottom),
+            normalized_to_pixel(p_a_t),
+            normalized_to_pixel(p_a_b),
             wall.color,
         );
         draw_line(
             frame,
-            normalized_to_pixel(p_b_top),
-            normalized_to_pixel(p_b_bottom),
+            normalized_to_pixel(p_b_t),
+            normalized_to_pixel(p_b_b),
             wall.color,
         );
 
@@ -324,6 +355,10 @@ fn draw_wall_system(
     }
 }
 
+fn draw_wall(frame: &mut [u8], a_t: Pixel, a_b: Pixel, b_t: Pixel, b_b: Pixel, state: AppState) {}
+
+fn draw_image(frame: &mut [u8], t_left: Pixel, image: DynamicImage) {}
+
 fn draw_line(frame: &mut [u8], a: Pixel, b: Pixel, color: Color) {
     for (x, y) in line_drawing::Bresenham::new((a.0, a.1), (b.0, b.1)) {
         draw_pixel(frame, Pixel(x, y), color);
@@ -340,7 +375,7 @@ fn absolute_to_pixel(v: Vec3) -> Pixel {
 fn normalized_to_pixel(v: Vec3) -> Pixel {
     Pixel(
         FRAC_WIDTH_2 as isize + (FRAC_WIDTH_2 as f32 * v.x).round() as isize,
-        FRAC_HEIGHT_2 as isize + (FRAC_HEIGHT_2 as f32 * v.y).round() as isize,
+        FRAC_HEIGHT_2 as isize - (FRAC_HEIGHT_2 as f32 * v.y).round() as isize,
     )
 }
 
